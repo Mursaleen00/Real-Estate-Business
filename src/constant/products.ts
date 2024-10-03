9b225a38495d96498ee10a0b0ed1d220882558ef
mursaleen-zenkoders <mursaleen@zenkoders.com>
2024-10-03 22:01:09 +0500

    `Feat`: - Creating Product card - Update Home page

---
+++ src/constant/products.ts
@@ -0,0 +1,69 @@
+import second from "../../public/images/property 1.png";
+import third from "../../public/images/property 2.png";
+import fourth from "../../public/images/property 3.png";
+import fifth from "../../public/images/property 2.png";
+import sixth from "../../public/images/property 3.png";
+import seventh from "../../public/images/property 1.png";
+
+export const products = [
+  {
+    title: "Seaside Serenity Villa",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    price: 100000,
+    image: second,
+    bathrooms: 2,
+    bedrooms: 3,
+    view: "360",
+  },
+  {
+    title: "Metropolitan Haven",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    price: 100000,
+    image: third,
+    bathrooms: 2,
+    bedrooms: 3,
+    view: "360",
+  },
+  {
+    title: "Rustic Retreat Cottage",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    price: 100000,
+    image: fourth,
+    bathrooms: 2,
+    bedrooms: 3,
+    view: "360",
+  },
+  {
+    title: "Metropolitan Haven",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    price: 100000,
+    image: fifth,
+    bathrooms: 2,
+    bedrooms: 3,
+    view: "360",
+  },
+  {
+    title: "Seaside Serenity Villa",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    price: 100000,
+    image: sixth,
+    bathrooms: 2,
+    bedrooms: 3,
+    view: "360",
+  },
+  {
+    title: "Rustic Retreat Cottage",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipisicing elit. lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    price: 100000,
+    image: seventh,
+    bathrooms: 2,
+    bedrooms: 3,
+    view: "360",
+  },
+];
