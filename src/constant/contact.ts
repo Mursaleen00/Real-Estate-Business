7aa228509c7664c775e2286f439e4a8e8622a331
Mursaleen00 <playessport@gmail.com>
2024-10-05 07:01:01 +0500

    `Feat`: - Create Contact Form - Create Input Component - Update Home

---
+++ src/constant/contact.ts
@@ -0,0 +1,38 @@
+export const contactData = [
+  {
+    label: "First Name",
+    placeholder: "Enter Your First Name",
+    type: "text",
+    name: "First Name",
+  },
+  {
+    label: "Last Name",
+    placeholder: "Enter Your Last Name",
+    type: "text",
+    name: "Last Name",
+  },
+  {
+    label: "Email",
+    placeholder: "Enter Your Email",
+    type: "email",
+    name: "Email",
+  },
+  {
+    label: "Phone Number",
+    placeholder: "Enter Your Phone Number",
+    type: "number",
+    name: "Phone Number",
+  },
+  {
+    label: "Inquiry Type",
+    placeholder: "Enter Your Inquiry Type",
+    type: "text",
+    name: "Inquiry Type",
+  },
+  {
+    label: "How Did You Hear About Us?",
+    placeholder: "Enter Your How Did You Hear About Us?",
+    type: "text",
+    name: "How Did You Hear About Us?",
+  },
+];
